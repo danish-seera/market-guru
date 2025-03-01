8a0a08fc460ee33e75d25a5307544fb1518f62c8
Danish <tux.danish@gmail.com>
2025-03-01 12:31:09 +0400

    added auth

--- src/main/java/com/stock_market/market_guru/response/HoldingsResponse.java
+++ src/main/java/com/stock_market/market_guru/response/HoldingsResponse.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -17,14 +19,23 @@ public class HoldingsResponse {
     private String broker;
     private String exchange;
     private String instrument;
+    @JsonProperty("company_name")
     private String companyName;
     private Integer quantity;
+    @JsonProperty("avg_price")
     private Integer avgPrice;
+    @JsonProperty("investment_value")
     private BigDecimal investmentValue;
+    @JsonProperty("current_price")
     private BigDecimal currentPrice;
+    @JsonProperty("current_value")
     private BigDecimal currentValue;
+    @JsonProperty("profit_loss")
     private BigDecimal profitLoss;
+    @JsonProperty("profit_loss_percentage")
     private BigDecimal profitLossPercentage;
+    @JsonProperty("created_at")
     private LocalDateTime createdAt;
+    @JsonProperty("updated_at")
     private LocalDateTime updatedAt;
 } 
